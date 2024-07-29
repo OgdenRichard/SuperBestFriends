f33df64a420b657e65385f76900f82c8584fa629
DerzoGit <journetalexandre.contact@gmail.com>
2024-07-29 14:55:21 +0200

    20240729 | Add to UserController GetAll()

---
+++ SuperBestFriends.API/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperBestFriends.Business.Abstractions;
+using SuperBestFriends.Business.DataTransfertObjects;
+
+namespace SuperBestFriends.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // Import du service d'utilisateur
+        private readonly IUserService userService;
+        public UsersController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+    
+        // Récupération de tous les utilisateurs
+        [HttpGet]
+        public ActionResult<IEnumerable<UserDto>> GetAll()
+        {
+            return this.Ok(this.userService.GetAll());
+        }
+    }
+}
