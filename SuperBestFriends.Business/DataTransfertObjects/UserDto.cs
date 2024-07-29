35f7478416ee0e591408c7eead16715eccc84ff5
DerzoGit <journetalexandre.contact@gmail.com>
2024-07-29 14:44:11 +0200

    20240729 | Clear useless namespace UserDto

--- SuperBestFriends.Business/DataTransfertObjects/UserDto.cs
+++ SuperBestFriends.Business/DataTransfertObjects/UserDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SuperBestFriends.Business.DataTransfertObjects
+﻿namespace SuperBestFriends.Business.DataTransfertObjects
 {
     public sealed class UserDto
     {
