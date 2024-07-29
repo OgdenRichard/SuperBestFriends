79d689a77264f35445a75717d393ad85bae13d94
DerzoGit <journetalexandre.contact@gmail.com>
2024-07-29 14:41:16 +0200

    20240729 | Mapping UserDto

---
+++ SuperBestFriends.Business/Extensions/MappingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using SuperBestFriends.Business.DataTransfertObjects;
+using SuperBestFriends.DAL.Entities;
+
+namespace SuperBestFriends.Business.Extensions
+{
+    public static class MappingExtensions
+    {
+        public static UserDto ToDto(this User user)
+        {
+            return new UserDto
+            {
+                UserId = user.UserId,
+                FirstName = user.FirstName,
+                LastName = user.LastName
+            };
+        }
+    }
+}
