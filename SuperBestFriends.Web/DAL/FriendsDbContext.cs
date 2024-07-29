e7f84a077a2a1e8de76bfdd4828f3606525e41bf
Richard Ogden <ogden.richard@gmail.com>
2024-07-29 11:15:27 +0200

    20240729 | migration users table

--- SuperBestFriends.Web/DAL/FriendsDbContext.cs
+++ SuperBestFriends.Web/DAL/FriendsDbContext.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SuperBestFriends.Web.DAL.Entities;
 
 namespace SuperBestFriends.Web.DAL
 {
     public sealed class FriendsDbContext : DbContext
     {
         public FriendsDbContext(DbContextOptions<FriendsDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
---
+++ SuperBestFriends.Web/Migrations/20240729091406_UsersTable.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SuperBestFriends.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Interests = table.Column<string>(type: "ntext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserUser",
+                columns: table => new
+                {
+                    FriendsOfUserId = table.Column<long>(type: "bigint", nullable: false),
+                    FriendsUserId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserUser", x => new { x.FriendsOfUserId, x.FriendsUserId });
+                    table.ForeignKey(
+                        name: "FK_UserUser_Users_FriendsOfUserId",
+                        column: x => x.FriendsOfUserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId");
+                    table.ForeignKey(
+                        name: "FK_UserUser_Users_FriendsUserId",
+                        column: x => x.FriendsUserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserUser_FriendsUserId",
+                table: "UserUser",
+                column: "FriendsUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserUser");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
