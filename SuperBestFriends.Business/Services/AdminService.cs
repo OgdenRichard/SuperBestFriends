5b44e6e31714eb425fcf468e02b5300cd5a00e24
DerzoGit <journetalexandre.contact@gmail.com>
2024-07-29 15:05:40 +0200

    20240729 | Create AdminService

---
+++ SuperBestFriends.Business/Services/AdminService.cs
@@ -0,0 +1,52 @@
+﻿using SuperBestFriends.Business.Abstractions;
+using SuperBestFriends.Business.DataTransfertObjects;
+using SuperBestFriends.DAL;
+
+namespace SuperBestFriends.Business.Services
+{
+    internal sealed class AdminService : IAdminService
+    {
+        // Import du DbContext
+        private readonly FriendsDbContext dbContext;
+        public AdminService(FriendsDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // Récupération de tous les utilisateurs
+        public List<UserAdminDto> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Récupération d'un utilisateur à partir de son ID
+        public UserAdminDto? GetById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Création d'un utilisateur
+        public Task<long> CreateAsync(UserAdminDto user)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Edition d'un utilisateur
+        public Task<long> UpdateAsync(UserAdminDto user)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Suppression d'un utilisateur à partir de son ID
+        public Task<bool> DeleteAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Vérification si un utilisateur existe bien à partir de son email (unique)
+        public Task<bool> UserExistsAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
