c4627fb073494dde1e47ea27d68ad33f9278cde1
DerzoGit <journetalexandre.contact@gmail.com>
2024-07-29 14:45:26 +0200

    20240729 | UserController GetAll()

--- SuperBestFriends.Business/Services/UserService.cs
+++ SuperBestFriends.Business/Services/UserService.cs
@@ -1,8 +1,29 @@
 ﻿using SuperBestFriends.Business.Abstractions;
+using SuperBestFriends.Business.DataTransfertObjects;
+using SuperBestFriends.Business.Extensions;
+using SuperBestFriends.DAL;
 
 namespace SuperBestFriends.Business.Services
 {
     internal sealed class UserService : IUserService
     {
+        // Import du DbContext
+        private readonly FriendsDbContext dbContext;
+        public UserService(FriendsDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // Récupération de tous les utilisateurs
+        public List<UserDto> GetAll()
+        {
+            return this.dbContext.Users.Select(user => user.ToDto()).ToList();
+        }
+
+        // Ajout d'un ami
+        public Task<UserDto> AddFriendAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
