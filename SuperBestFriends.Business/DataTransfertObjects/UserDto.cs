e367f9b30ae8acc9f23c76f634c2ce5ca9c099db
DerzoGit <journetalexandre.contact@gmail.com>
2024-07-29 14:43:09 +0200

    20240729 | Add UserDto

---
+++ SuperBestFriends.Business/DataTransfertObjects/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperBestFriends.Business.DataTransfertObjects
+{
+    public sealed class UserDto
+    {
+        public long UserId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
