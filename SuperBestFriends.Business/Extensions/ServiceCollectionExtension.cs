2ae7a057babca024a9bc2219c0ae8727689d0223
DerzoGit <journetalexandre.contact@gmail.com>
2024-07-29 14:21:23 +0200

    20240729 | AddTransient UserService

---
+++ SuperBestFriends.Business/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SuperBestFriends.Business.Abstractions;
+using SuperBestFriends.Business.Services;
+
+namespace SuperBestFriends.Business.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddBusiness(this IServiceCollection services)
+        {
+            services.AddTransient<IUserService, UserService>();
+            return services;
+        }
+    }
+}
