b4862ceda840067cc7312d74c901dfb539c3aa5e
Richard Ogden <ogden.richard@gmail.com>
2024-07-29 10:28:09 +0200

    Ajoutez des fichiers projet.

---
+++ SuperBestFriends.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SuperBestFriends.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
