a2808d425b11c13a259de5238cd3623273d5cc0c
DerzoGit <journetalexandre.contact@gmail.com>
2024-07-30 10:26:09 +0200

    20240730 | Add Address field & migrations

--- SuperBestFriends.DAL/Migrations/20240730082309_AddAddress.Designer.cs
+++ SuperBestFriends.DAL/Migrations/20240730082309_AddAddress.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SuperBestFriends.DAL;
 
@@ -11,9 +12,11 @@ using SuperBestFriends.DAL;
 namespace SuperBestFriends.DAL.Migrations
 {
     [DbContext(typeof(FriendsDbContext))]
-    partial class FriendsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240730082309_AddAddress")]
+    partial class AddAddress
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SuperBestFriends.DAL/Migrations/20240730082309_AddAddress.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SuperBestFriends.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Users",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
